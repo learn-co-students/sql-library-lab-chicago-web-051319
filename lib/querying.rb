f1ba83469ea880384320d64ecead698299d4f345
github username <github email address>
2019-05-22 14:25:48 -0500

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ def select_value_and_count_of_most_prolific_species
   FROM characters
   GROUP BY species
   ORDER BY species DESC
-  LIMIT 1; "
+  LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
